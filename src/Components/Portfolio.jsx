ce0d9deeba6e0a6636bc3adfe4d4bcb45e93d7ba
Dominiq Barbero <mr.dom.barbero@gmail.com>
2026-01-17 15:03:32 +0000

    Use WebP for portfolio image and update Parcel

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import imageUrl from "url:../images/workspace-unsplash.jpg";
+import imageUrl from "url:../images/workspace-unsplash.webp";
 
 const imageAltText = "Workspace with a laptop, notebook, cup of coffee and other items on desk";
 
